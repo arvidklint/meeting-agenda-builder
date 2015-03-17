0a45a4edd1dfd2e84e5191c3a1b00c22f98d275e
Arvid Sätterkvist <arvidsat@gmail.com>
2015-03-17 19:34:58 +0100

    Solved merge conflicts (hopefully...)

--- meeting-agenda-builder/main.js
+++ meeting-agenda-builder/main.js
@@ -1,13 +1,9 @@
 if (Meteor.isClient) {
 	Meteor.subscribe("schedules");
 
-<<<<<<< HEAD
 	Session.set("parkedActivityModal", false);
 
-	Template.body.helpers({
-=======
 	Template.parkedActivitiesView.helpers({
->>>>>>> 9a5018de4f0a97c581466ad0b354a5e348ccc6cf
 		parkedActivities: function() {
 			return getParkedActivities();
 		}
@@ -15,36 +11,9 @@ if (Meteor.isClient) {
 
 	Template.parkedActivitiesView.events({
 		"click #addParkedActivity": function() {
-<<<<<<< HEAD
-			// test_activity = {
-			// 	name: "Mats",
-			// 	length: "60",
-			// 	type: "Presentation",
-			// 	description: "Mats ska berätta om föroreningar orsakade av Al Gore"
-			// };
-			//
-			// Meteor.call("addParkedActivity", test_activity, null);
 			Session.set("parkedActivityModal", true);
 		}
 	});
-=======
-			test_activity = {
-				name: "Mats",
-				length: "60",
-				type: "presentation",
-				description: "Mats ska berätta om föroreningar orsakade av Al Gore"
-			};
-
-			Meteor.call("addParkedActivity", test_activity, null);
-		},
-		"click #addParkedActivityAt2": function() {
-			test_activity = {
-				name: "Nummer2aktiviteten",
-				length: "360",
-				type: "group_discussion",
-				description: "Hängringar om te"
-			};
->>>>>>> 9a5018de4f0a97c581466ad0b354a5e348ccc6cf
 
 	Template.newActivityView.events({
 		"click #closeModal": function() {
@@ -52,13 +21,12 @@ if (Meteor.isClient) {
 		}
 	});
 
-<<<<<<< HEAD
 	Template.newActivityView.helpers({
 		addParkedActivityModal: function() {
 			return Session.get("parkedActivityModal");
 		}
-	})
-=======
+	});
+
 	Template.daysView.helpers({
 		days: function() {
 			return getDays();
@@ -75,8 +43,5 @@ if (Meteor.isClient) {
 		startTimeHuman: function() {
 			return this.startTime + 10;
 		}
-	})
-
-	
->>>>>>> 9a5018de4f0a97c581466ad0b354a5e348ccc6cf
+	});
 }
