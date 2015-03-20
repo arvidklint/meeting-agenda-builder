9606ffeff597fb928dbd4d530e7a737e3d007086
Arvid Sätterkvist <arvidsat@gmail.com>
2015-03-20 14:29:33 +0100

    Placed the activity code in a separate file.

--- meeting-agenda-builder/main.js
+++ meeting-agenda-builder/main.js
@@ -1,50 +1,6 @@
 if (Meteor.isClient) {
 	Meteor.subscribe("schedules");
 
-	Session.set("parkedActivityModal", false);
-
-	Template.parkedActivitiesView.helpers({
-		parkedActivities: function() {
-			return getParkedActivities();
-		}
-	});
-
-	Template.parkedActivitiesView.events({
-		"click #addParkedActivity": function() {
-			Session.set("parkedActivityModal", true);
-		}
-	});
-
-	Template.newActivityView.events({
-		"click #closeModal": function() {
-			Session.set("parkedActivityModal", false);
-		},
-		"submit .newActivity": function(event) {
-			var n = event.target.name.value;
-			var l = event.target.length.value;
-			var t = event.target.type.value;
-			var d = event.target.description.value;
-
-			var activity = {
-				name: n,
-				length: l,
-				type: t,
-				description: d
-			}
-
-			Meteor.call("addParkedActivity", activity, null);
-
-			Session.set("parkedActivityModal", false);
-			return false;
-		}
-	});
-
-	Template.newActivityView.helpers({
-		addParkedActivityModal: function() {
-			return Session.get("parkedActivityModal");
-		}
-	});
-
 	Template.daysView.helpers({
 		days: function() {
 			return getDays();
